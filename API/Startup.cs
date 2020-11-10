c3a09baea4ab8d2a728b302a1e3101d3346aafb1
Stepan Vasylevskyi <stephen.v.xi@gmail.com>
2020-11-10 20:33:55 +0100

    Skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,12 @@ namespace API
                     opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
                 }
             );
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -44,6 +50,8 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
